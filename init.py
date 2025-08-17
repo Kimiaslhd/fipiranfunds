91baa73655cc8caad9f58608ee846d9a2e3f8e01
Kimia Salehi Delarestaghi <113443561+Kimiaslhd@users.noreply.github.com>
2025-08-17 14:42:59 +0330

    Update init.py

--- init.py
+++ init.py
@@ -1,15 +1,10 @@
-"""
-FipiranFunds - A Python library for fetching Iranian fund data.
+"""Fipiran Funds - A library for fetching Iranian fund data."""
 
-Author: Kimia Salehy Delarestaghy
-Email: kimiaslhd@gmail.com
-LinkedIn: https://www.linkedin.com/in/kimia-salehy-delarestaghy/
-"""
+__version__ = "0.1.0"
+__author__ = "K.Salehi"
+__description__ = "A Python library for fetching Iranian fund data from Fipiran API"
 
 from .core import export_fund_data
 from .api import *
 
-__version__ = "0.1.0"
-__author__ = "Kimia Salehy Delarestaghy"
-__email__ = "kimiaslhd@gmail.com"
-__all__ = ["export_fund_data"]
+__all__ = ['export_fund_data'] 
